03dd413c500481f7a890832e5e1303b3cc6dc277
DainJung <wjdekdls3693@gmail.com>
2023-07-16 22:28:45 +0900

    study velocity package

--- shake_count_app/lib/my_home_page.dart
+++ shake_count_app/lib/my_home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shake/shake.dart';
+import 'package:shake_count_app/red_box.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -10,12 +12,14 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   int _counter = 0;
+  late ShakeDetector detector;
 
   @override
   void initState() {
-    ShakeDetector detector = ShakeDetector.autoStart(
+    WidgetsBinding.instance.addObserver(this);
+    detector = ShakeDetector.autoStart(
       onPhoneShake: () {
         setState(() {
           _counter++;
@@ -33,25 +37,52 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              '흔들어서 카운트를 올려보세요.',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.displayLarge,
-            ),
-          ],
-        ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const RedBox(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const RedBox()
+                  .box
+                  .padding(const EdgeInsets.all(30))
+                  .color(Colors.blue)
+                  .make(),
+              '흔들어서 카운트를 올려보세요.'
+                  .text
+                  .black
+                  .bold
+                  .size(20)
+                  .isIntrinsic
+                  .makeCentered()
+                  .box
+                  .withRounded(value: 50)
+                  .color(Colors.green)
+                  .height(150)
+                  .make()
+                  .pSymmetric(h: 20, v: 50),
+              const RedBox(),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+            ],
+          ),
+          const RedBox(),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
@@ -59,5 +90,21 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    switch (state) {
+      case AppLifecycleState.resumed:
+        detector.startListening();
+        break;
+      case AppLifecycleState.inactive:
+        break;
+      case AppLifecycleState.paused:
+        detector.stopListening();
+        break;
+      case AppLifecycleState.detached:
+        break;
+    }
   }
 }
