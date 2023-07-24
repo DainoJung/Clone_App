4410e7bb9084ace2dffd48e3583b737a39f3ab17
DainJung <wjdekdls3693@gmail.com>
2023-07-24 21:34:45 +0900

    update 3.02.12

--- fast_app_base-main/lib/screen/main/tab/stock/f_stock.dart
+++ fast_app_base-main/lib/screen/main/tab/stock/f_stock.dart
@@ -1,3 +1,5 @@
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/common/widget/w_image_button.dart';
 import 'package:flutter/material.dart';
 
 class StockFragment extends StatefulWidget {
@@ -10,6 +12,36 @@ class StockFragment extends StatefulWidget {
 class _StockFragmentState extends State<StockFragment> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          actions: [
+            ImageButton(
+              onTap: () {
+                context.showSnackbar('검색');
+              },
+              imagePath: '$basePath/icon/stock_search.png',
+            ),
+            ImageButton(
+              onTap: () {
+                context.showSnackbar('캘린더');
+              },
+              imagePath: '$basePath/icon/stock_calendar.png',
+            ),
+            ImageButton(
+              onTap: () {
+                context.showSnackbar('설정');
+              },
+              imagePath: '$basePath/icon/stock_settings.png',
+            ),
+            const SliverToBoxAdapter(
+                child: Column(
+              children: [],
+            )),
+          ],
+        )
+      ],
+    );
   }
 }
